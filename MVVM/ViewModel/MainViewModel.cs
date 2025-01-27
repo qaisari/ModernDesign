2a4053a33db483d7bfde2367eababbc337990d55
Ayman Qaisari <qaisariayman@gmail.com>
2025-01-27 22:24:09 +0100

    new updates

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -9,9 +9,26 @@ namespace ModernDesign.MVVM.ViewModel
 {
     class MainViewModel : ObservableObject
     {
+
+        public HomeViewModel HomeVm{ get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
         public MainViewModel()
         {
-
+            HomeVm = new HomeViewModel();
+            CurrentView = HomeVm;
         }
     }
 }
